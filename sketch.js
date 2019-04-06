1315bf4b03aa4689788f3e4820d8153a62f362d3
nikalsh <niklas@helander.se>
2019-04-06 17:59:56 +0200

    working ruleset and tick

--- sketch.js
+++ sketch.js
@@ -1,80 +1,118 @@
+const width = 600
+const height = 600
+const cellDim = 20
+const rowLen = width / cellDim
+const coLen = height / cellDim
 
 class Grid {
 	constructor() {
 		this.cells = []
+		this.dirs = [
+			1,
+			-1,
+			-rowLen,
+			rowLen,
+			1 - rowLen,
+			-1 - rowLen,
+			1 + rowLen,
+			-1 + rowLen,
+		]
 	}
 
-	addCell (x, y, w, h) {
-		this.cells.push({ row: (x / w), col: (y / h), w, h, shape: rect(x, y, w, h), pop: false })
-	}
-
-	colorAllCells () {
-		this.cells.forEach(e => {
-			fill(233, 0, 255)
-			rect(e.x, e.y, e.w, e.h)
-			e.pop = true
+	addCell(x, y, w, h) {
+		this.cells.push({
+			x,
+			y,
+			row: x / w,
+			col: y / h,
+			w,
+			h,
+			shape: rect(x, y, w, h),
+			populated: false,
+			lives: false,
 		})
 	}
 
-	nextGeneration () {
+	tick() {
+		this.nextGeneration()
 		this.cells.forEach(e => {
-
-
-
-
+			e.lives ? this.birthCell(e) : this.killCell(e)
 		})
-
 	}
 
-	startPattern () {
-
+	nextGeneration() {
 		this.cells.forEach(e => {
+			let index = this.cells.indexOf(e)
+			let adjacent = 0
 
-			if (e.y === 260) {
-				this.colorCells(e);
-
-
+			try {
+				for (let i = 0; i < this.dirs.length; i++) {
+					if (this.cells[index + this.dirs[i]].populated) {
+						adjacent++
+					}
+				}
+			} catch (err) {
+				// out of array bounds
 			}
-
+			//ruleset -
+			//living cells
+			if (e.populated) {
+				if (adjacent < 2) {
+					e.lives = false
+				} else if (adjacent > 3) {
+					e.lives = false
+				} else if (adjacent === 2 || adjacent === 3) {
+					e.lives = true
+				}
+				//dead cells
+			} else {
+				if (adjacent === 3) {
+					e.lives = true
+				}
+			}
 		})
-
 	}
 
-	colorCells (e) {
+	birthCell(e) {
 		fill(233, 0, 255)
 		rect(e.x, e.y, e.w, e.h)
-		e.painted = true
+		e.populated = true
 		fill(255, 255, 255)
 	}
 
+	killCell(e) {
+		fill(255, 255, 255)
+		rect(e.x, e.y, e.w, e.h)
+		e.populated = false
+		fill(255, 255, 255)
+	}
+
+	initPattern() {
+		this.cells.forEach(e => {
+			if (e.col === 10) {
+				if (e.row > 10 && e.row < 20) {
+					this.birthCell(e)
+				}
+			}
+		})
+	}
 }
 
 var grid = new Grid()
-const width = 600;
-const height = 600;
 
-function setup () {
-
-	createCanvas(600, 600);
-
+function setup() {
+	createCanvas(width, height)
 	background(0, 0, 0)
-
-}
-
-let x = 0;
-function draw () {
-
-
-	let x = 0;
-	cellDim = 20
+	let x = 0
 	for (let y = 0; y < height; y += cellDim) {
 		for (let x = 0; x < width; x += cellDim) {
-
 			this.grid.addCell(x, y, cellDim, cellDim)
 		}
 	}
+	this.grid.initPattern()
+}
 
-	this.grid.startPattern()
-	setInterval(this.grid.nextGeneration(), 10000);
-
+function draw() {
+	this.grid.initPattern()
+	this.grid.tick()
 }
